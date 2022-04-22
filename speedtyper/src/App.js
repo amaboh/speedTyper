0e729bdc5cb136424e33115024b2d0337ffe0d0d
amaboh <amabocanon@gmail.com>
2022-04-22 01:57:25 +0100

    updates on the reset game when start button is press and and disabled on every new game

--- speedtyper/src/App.js
+++ speedtyper/src/App.js
@@ -1,21 +1,40 @@
 import {useState, useEffect} from "react"
 
 function App() {
+
+const TIME_REMAINING = 6
+
 const [ textField, setTextField] = useState("")
-const [ countDown, setCountDown] = useState(13)
+const [ countDown, setCountDown] = useState(TIME_REMAINING)
+const [startGame, setStartGame] = useState(false)
+const [wordCount, setWordCount] = useState(0)
 
 function handleChange (event){
     const {value} = event.target
     setTextField(value)
 }
 
+function gameOn(){
+  setStartGame(true)
+  setCountDown(TIME_REMAINING)
+  setTextField("")
+  
+}
+
+function endGame(){
+  setStartGame(false)
+  setWordCount(countWords(textField))
+}
+
 useEffect(()=>{
   setTimeout(()=>{
-    if (countDown > 0 ){
+    if (startGame && countDown > 0 ){
       setCountDown(prevTime => prevTime - 1)
+    } else if(countDown === 0){
+      endGame()
     }
   }, 1000)
-}, [countDown])
+}, [countDown, startGame])
 
 function countWords(str){
   const numWords = str.trim().split("")
@@ -23,8 +42,6 @@ function countWords(str){
   return filterWords.length
 }
 
-
- 
 
 
 console.log(textField)
@@ -35,8 +52,8 @@ console.log(textField)
                 onChange={handleChange}
                   />
       <h4>Time remaining: {countDown}</h4>
-      <button>Start</button>
-      <h1>Word Count: {countWords(textField)}</h1>
+      <button onClick={gameOn()}>Start</button>
+      <h1>No. of Character: {wordCount}</h1>
     </div>
   );
 }
